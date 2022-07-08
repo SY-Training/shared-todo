053e006bdb41370d8ec80ec71feb01be8e5a4fee
S-Y <sy-unimportant@outlook.com>
2022-07-08 17:01:33 +0100

    Additinal start config. App now launches via server.js

--- svelte-express-app/server/server.js
+++ svelte-express-app/server/server.js
@@ -1,4 +1,14 @@
 const express = require("express");
 const sanitizeHtml = require("sanitize-html");
+const path = require("path");
 
 const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+});
+app.listen(port, () => {
+  console.log(`Server is up at port ${port}`);
+});
